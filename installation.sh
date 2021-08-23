3a503a511bdeabbb85659f6e498596de7374475f
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-08-22 23:12:48 -0700

    begin collecting all my installations in one file

--- installation.sh
+++ installation.sh
@@ -1,3 +1,7 @@
+# install bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
 # create a symlink for all of your configuration files
 FILES=(./repo/{.vim,.vimrc,.zshrc})
 for f in ${FILES[*]}; do
@@ -8,5 +12,13 @@ for f in ${FILES[*]}; do
 done
 unset f;
 
+# download vim plug, package manager for vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+# run the brew file, only works if the above loop was run successfully
+brew bundle
+
+
+
