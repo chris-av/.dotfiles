f08a99a67544f2e9bb920073b8c84cb7b36b6ed1
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-10-19 18:51:22 -0700

    fix symlink operation

--- utilities/nvim.sh
+++ utilities/nvim.sh
@@ -1,10 +1,12 @@
 echo "preparing nvim"
+echo "running from $(pwd)"
 echo ""
 echo ""
 
 
 NVIM_CONFIG=./nvim
-if [ -r NVIM_CONFIG ]; then
+echo "nvim config here : $NVIM_CONFIG"
+if [ -d "$NVIM_CONFIG" ]; then
   echo "creating symlink for nvim"
   ln -sr $NVIM_CONFIG ~/.config
 fi
@@ -13,6 +15,11 @@ unset NVIM_CONFIG;
 
 echo "download packer for nvim ... "
 echo ""
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+PACKER_DIR=~/.local/share/nvim/site/pack
+if [ ! -d "$PACKER_DIR" ]; then
+  echo "downloading packer"
+  mkdir -p ~/.local/share/nvim/site/pack
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
+unset PACKER_DIR
 
