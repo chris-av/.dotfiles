ff3a532af7e24bfe872b858392b25a08145f47e7
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-11-15 22:26:39 -0800

    update nvim-autopairs, setup deprecated

--- nvim/lua/completions.lua
+++ nvim/lua/completions.lua
@@ -1,4 +1,7 @@
 local cmp = require'cmp'
+require'nvim-autopairs'.setup({
+  enable_check_bracket_line = false
+})
 
 cmp.setup({
 
@@ -13,7 +16,10 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({
+      select = true,
+      behavior = cmp.ConfirmBehavior.replace
+    }),
     ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
     ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })
   },
@@ -75,20 +81,14 @@ cmp.setup({
 
 })
 
-require('nvim-autopairs').setup({
-  enable_check_bracket_line = false
-})
+local cmp_autopairs = require'nvim-autopairs.completion.cmp'
+cmp.event:on(
+  "confirm_done",
+  cmp_autopairs.on_confirm_done()
+)
 
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-  auto_select = true, -- automatically select the first item
-  insert = false, -- use insert confirm behavior instead of replace
-  map_char = { -- modifies the function or method delimiter by filetypes
-    all = '(',
-    tex = '{'
-  }
-})
+
+
 
 require('telescope').setup {}
 
