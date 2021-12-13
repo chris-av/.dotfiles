9b920065ccb226baba22d8e1690d228689dd3668
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-12-12 16:34:50 -0800

    organize packer configs

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -3,24 +3,31 @@ local packer = require 'packer'
 packer.startup(function()
   local use = packer.use
   use 'wbthomason/packer.nvim'
+
+  -- lsp
+  use 'neovim/nvim-lspconfig'
+  use 'kabouzeid/nvim-lspinstall'
   use 'nvim-treesitter/nvim-treesitter'
-  use 'maxmellon/vim-jsx-pretty'
-  use 'alvan/vim-closetag'
-  use 'preservim/nerdtree'
-  use 'tiagofumo/vim-nerdtree-syntax-highlight'
-  use 'ryanoasis/vim-devicons'
+
+  -- interface
   use 'vim-airline/vim-airline'
   use 'vim-airline/vim-airline-themes'
   use 'tpope/vim-fugitive'
-  use 'neovim/nvim-lspconfig'
-  use 'kabouzeid/nvim-lspinstall'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'windwp/nvim-autopairs'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip'
   use 'mhinz/vim-signify'
+
+  -- completion
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'windwp/nvim-autopairs'
+  use 'L3MON4D3/LuaSnip'
+
+
+
+  -- telescope
   use {
     'nvim-telescope/telescope.nvim',
     requires = {
@@ -29,4 +36,12 @@ packer.startup(function()
       { 'sharkdp/fd' }
     }
   }
+
+  -- non neovim plugins
+  use 'maxmellon/vim-jsx-pretty'
+  use 'alvan/vim-closetag'
+  use 'preservim/nerdtree'
+  use 'tiagofumo/vim-nerdtree-syntax-highlight'
+  use 'ryanoasis/vim-devicons'
+
 end)
