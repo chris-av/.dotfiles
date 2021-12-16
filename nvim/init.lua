5ef66a7344f78056de4a64ff8be24e2297c36dea
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-12-15 19:28:06 -0800

    update gui colors and neovide settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,16 +27,16 @@ vim.o.splitright = true
 
 -- try out some colors
 -- if you define invalid colors, the pmenu ends up looking aweseome???
-vim.cmd([[ highlight Pmenu ctermfg=black ctermbg=DarkGreen ]])
-vim.cmd([[ highlight PmenuSel ctermfg=white ctermbg=DarkGreen ]])
-vim.cmd([[ highlight NormalFloat guifg=black guibg=black ]])
-vim.cmd([[ highlight FloatBorder guifg=black guibg=black ]])
+vim.cmd([[ highlight Pmenu ctermfg=black ctermbg=DarkGreen guifg=black guibg=LightGreen ]])
+vim.cmd([[ highlight PmenuSel ctermfg=white ctermbg=DarkGreen guifg=white guibg=LightGreen ]])
+vim.cmd([[ highlight NormalFloat ctermfg=white ctermbg=black guifg=white guibg=black ]])
+vim.cmd([[ highlight FloatBorder ctermfg=white ctermbg=black guifg=white guibg=black ]])
 -- vim.cmd([[ highlight NormalFloat ctermbg=black ]])
 -- vim.cmd([[ highlight NormalFloat ctermbg=black ]])
 vim.cmd([[ highlight CursorLine cterm=NONE ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE ]])
-vim.cmd([[ highlight LineNr ctermbg=NONE ]])
-vim.cmd([[ highlight CursorLineNR cterm=bold ]])
-vim.cmd([[ highlight VertSplit ctermbg=DarkGreen ctermfg=black guifg=black guibg=black ]])
+vim.cmd([[ highlight LineNr ctermbg=NONE guibg=NONE guifg=LightGreen ]])
+vim.cmd([[ highlight CursorLineNR cterm=bold gui=bold ]])
+vim.cmd([[ highlight VertSplit ctermbg=DarkGreen ctermfg=black ctermbg=black guifg=black guibg=black ]])
 
 vim.cmd([[ highlight SignifySignAdd ctermfg=green guifg=#00ff00 cterm=NONE gui=NONE ]])
 vim.cmd([[ highlight SignifySignDelete ctermfg=red guifg=#00ff00 cterm=NONE gui=NONE ]])
@@ -70,6 +70,15 @@ require 'completions'
 require 'diagnostics'
 require 'packages'
 require 'lsp'
+
+
+if vim.api.nvim_eval("exists('g:neovide')") == 1 then
+  -- -- for debugging purposes
+  -- vim.cmd [[ echo "running neovide!" ]]
+  vim.g.material_style = 'deep ocean'
+  vim.cmd[[ colorscheme material ]]
+  vim.cmd[[ set guifont=Hack\ Nerd\ Font:h11 ]]
+end
 
 
 
