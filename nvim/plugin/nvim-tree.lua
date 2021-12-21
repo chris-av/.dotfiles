de67def09c354be8f2669b0dfeba74222ee6c5dd
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-12-20 23:25:13 -0800

    replace nerdtree.lua with nvim-tree.lua

--- nvim/plugin/nvim-tree.lua
+++ nvim/plugin/nvim-tree.lua
@@ -179,3 +179,13 @@ require'nvim-tree'.setup {
 }
 
 
+
+-- to ensure easy navigation
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-r>', ':mod<CR>', { noremap = true, silent = true })
+
+
+
