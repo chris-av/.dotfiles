cd3994d882133b50a5fedf7cabf4a09a3a8aaf08
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2022-01-08 00:13:00 -0800

    move settings to lua/configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,58 +1,14 @@
 local vim = vim
 local fn = vim.fn
 
--- make sure iTerm2 has "report mouse wheel events" turned off
-vim.o.mouse = 'a'
-vim.o.syntax = 'on'
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-vim.o.expandtab = true
-vim.o.number = true
-vim.bo.autoindent = true
-vim.bo.smartindent = true
-vim.o.backspace = 'indent,eol,start'
-vim.o.encoding = 'UTF-8'
-vim.cmd('highlight clear SignColumn')
-vim.o.showmatch = true
-vim.o.completeopt = 'menuone,noinsert,noselect'
-vim.wo.wrap = false
-vim.o.sidescroll=1
-vim.o.shortmess= vim.o.shortmess .. 'c'
-vim.o.guicursor = 'i:block,a:blinkon100'
-vim.bo.swapfile = true
-vim.o.cursorline = true
-vim.o.splitbelow = true
-vim.o.splitright = true
-vim.g.mapleader = " "
-vim.o.showmode = false
-vim.o.scrolloff = 8
-vim.o.sidescrolloff = 8
+-- highlight groups, options, etc
+require('configs')
 
 
--- try out some colors
--- if you define invalid colors, the pmenu ends up looking aweseome???
-vim.cmd([[ highlight Pmenu ctermfg=black ctermbg=DarkGreen guifg=black guibg=LightGreen ]])
-vim.cmd([[ highlight PmenuSel ctermfg=white ctermbg=DarkGreen guifg=white guibg=LightGreen ]])
-vim.cmd([[ highlight NormalFloat ctermfg=white ctermbg=black guifg=white guibg=black ]])
-vim.cmd([[ highlight FloatBorder ctermfg=white ctermbg=black guifg=white guibg=black ]])
--- vim.cmd([[ highlight NormalFloat ctermbg=black ]])
--- vim.cmd([[ highlight NormalFloat ctermbg=black ]])
-vim.cmd([[ highlight CursorLine cterm=NONE ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE ]])
-vim.cmd([[ highlight LineNr ctermbg=NONE guibg=NONE guifg=LightGreen ]])
-vim.cmd([[ highlight CursorLineNR cterm=bold gui=bold ]])
-vim.cmd([[ highlight VertSplit ctermbg=DarkGreen ctermfg=black ctermbg=black guifg=black guibg=black ]])
-
-vim.cmd([[ highlight SignifySignAdd ctermfg=green guifg=#00ff00 cterm=NONE gui=NONE ]])
-vim.cmd([[ highlight SignifySignDelete ctermfg=red guifg=#00ff00 cterm=NONE gui=NONE ]])
-vim.cmd([[ highlight SignifySignChange ctermfg=yellow guifg=#00ff00 cterm=NONE gui=NONE ]])
-
-vim.api.nvim_set_keymap('i', '<F1>', '<Esc>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('', '<F1>', '<nop>', { noremap = true, silent = true })
-
-
+-- indent on git files
 vim.cmd('au FileType gitcommit setlocal tw=72')
 
+-- use template html file
 vim.cmd([[
 augroup skeletons
   au!
@@ -75,7 +31,7 @@ require 'completions'
 require 'diagnostics'
 require 'packages'
 require 'lsp'
-require 'dbg'
+require 'debugger'
 
 
 
