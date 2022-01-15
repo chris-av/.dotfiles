aa099be0bfd3c8528fde89e7b3db6ad62861200d
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2022-01-14 22:23:19 -0800

    update nvim installation
    replace symlink of local nvim with git cloning my new neovim GitHub
    repository. Also update the script output so that it flows better

--- utilities/nvim.sh
+++ utilities/nvim.sh
@@ -1,25 +1,30 @@
 echo "preparing nvim"
-echo "running from $(pwd)"
 echo ""
 echo ""
 
 
-NVIM_CONFIG=./nvim
+NVIM_CONFIG=~/.config/nvim
 echo "nvim config here : $NVIM_CONFIG"
-if [ -d "$NVIM_CONFIG" ]; then
+if [ ! -d "$NVIM_CONFIG" ]; then
   echo "download my neovim github repo"
   git clone git@github.com:chris-av/nvim ~/.config/nvim
+else
+  echo "nvim already set up! ^^"
 fi
 unset NVIM_CONFIG;
 
-
-echo "download packer for nvim ... "
 echo ""
+echo ""
+
+echo "check packer for nvim ... "
 PACKER_DIR=~/.local/share/nvim/site/pack
 if [ ! -d "$PACKER_DIR" ]; then
   echo "downloading packer"
   mkdir -p ~/.local/share/nvim/site/pack
   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+else
+  echo "packer directory already setup : ${PACKER_DIR}"
 fi
 unset PACKER_DIR
 
+echo ""
