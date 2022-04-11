488fed8c6ff387f663b42519d899dbfa2fe43326
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-04-10 23:16:34 -0700

    init linux installation script, empty for now

---
+++ utilities/linux-installation.sh
@@ -0,0 +1,31 @@
+# update package manager
+
+# install packages that we will need
+
+
+# install git
+# install zsh
+# install zsh-syntax-highlighting
+# install heroku
+# install curl
+# install tree-sitter
+# install neovim
+# install lua
+# install luajit
+# install ruby
+# install youtube-dl
+# install bat
+# install ninja
+# install ffmpeg
+# install fd
+# install mongosh
+# install tmux
+
+
+## UPDATE if already on the system
+# git
+# bash
+# vim
+# python
+
+
