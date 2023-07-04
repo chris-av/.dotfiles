e4a2ecc16f9498f8010031c29181fa460f2c57f6
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-07-03 21:09:11 -0700

    lsp should be handled by mason

--- utilities/language-servers/tsserver.sh
+++
@@ -1,4 +0,0 @@
-echo "installing typescript language server ..."
-echo ""
-echo ""
-npm install -g typescript typescript-language-server
--- utilities/language-servers/zig-lang-server.sh
+++
@@ -1,9 +0,0 @@
-ZIG_CONFIG=$HOME/zls
-
-# if no zls directory in home, then create the directory run the script and add to path
-if [ ! -d $ZIG_CONFIG ]; then
-  mkdir $HOME/zls
-  cd $HOME/zls
-  curl -L https://github.com/zigtools/zls/releases/download/0.9.0/x86_64-macos.tar.xz | tar -xJ --strip-components=1 -C .
-  cd -
-fi
