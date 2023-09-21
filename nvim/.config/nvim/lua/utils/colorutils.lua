5a386a9da8f858643f09b3893dbacd33b766b8b9
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-09-20 18:57:03 -0700

    create custom util for determining color

---
+++ nvim/.config/nvim/lua/utils/colorutils.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+
+M.theme_table = {
+  ["Material Deep Ocean"] = "material",
+  ["Catppuccin"] = "catppuccin",
+  ["Embark"] = "embark",
+  ["Coal"] = "coal",
+  ["Matrix"] = "matrix",
+  ["Dark"] = "dark",
+  ["Snazzy"] = "snazzy",
+}
+
+M.set_colorscheme = function()
+  local term_prof = os.getenv('TERM_PROFILE')
+  if (term_prof == nil) then
+    vim.cmd " colorscheme dark "
+    return
+  else
+    local colorscheme = M.theme_table[term_prof]
+    vim.cmd(" colorscheme " .. colorscheme)
+    return
+  end
+end
+
+
+return M
+
