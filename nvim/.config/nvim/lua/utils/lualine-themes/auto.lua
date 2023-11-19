e072a19b11e515ec1bd0da74d5b4b1706c160a02
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-11-18 23:10:48 -0800

    define an auto config

---
+++ nvim/.config/nvim/lua/utils/lualine-themes/auto.lua
@@ -0,0 +1,11 @@
+return {
+  theme = "auto",
+  components = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics", },
+    lualine_c = { "navic", },
+    lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_y = {},
+    lualine_z = { "location" }
+  }
+}
