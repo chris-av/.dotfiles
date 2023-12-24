93bb8400cebcc6936f099cd7aac5bcf74ea014d2
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-12-24 01:27:25 -0800

    send signal to nvim when changing wallpapers

--- hyprland/.config/hypr/scripts/change-wallpaper.sh
+++ hyprland/.config/hypr/scripts/change-wallpaper.sh
@@ -41,4 +41,5 @@ fi
 swww img -t wipe --transition-step 190 --transition-duration 1.3 $prof_image
 kitty +kitten themes --dump-theme $prof_theme > ~/.config/kitty/tmp/current-theme.conf
 killall -SIGUSR1 kitty
+killall -SIGUSR1 nvim
 
