c273ec213f9416f48fe7a1c17d758cc8cff611e1
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-01-03 18:29:36 -0800

    silicon config

--- nvim/.config/nvim/lua/plugins/config/silicon.lua
+++ nvim/.config/nvim/lua/plugins/config/silicon.lua
@@ -1,8 +1,37 @@
-require('silicon').setup({
-  font = 'FantasqueSansMono Nerd Font=16',
-  theme = 'Monokai Extended',
-})
+local silicon = require('silicon')
 
--- -- use this nifty shortcut when silicon works
--- vim.api.nvim_set_keymap('n', '<leader>ss', ':Silicon!<CR>', { noremap = true, silent = true })
+-- create folder that will contain the silicon screenshots
+local dir = vim.fn.expand("~/Desktop/silicon/")
+if vim.fn.isdirectory(dir) == 0 then
+  print("why is this printing ?? ")
+  vim.notify("did not find " .. dir, vim.log.levels.ERROR)
+  vim.notify("creating dir " .. dir, vim.log.levels.INFO)
+  vim.fn.mkdir(dir)
+end
+
+
+local timefmt = os.date("%Y-%m-%d--%H:%M:%S")
+local filename = "silicon-" .. timefmt .. ".png"
+local outpath = dir .. filename
+
+silicon.setup({
+  theme = "auto",
+  output = outpath,
+  bgColor = vim.g.terminal_color_5,
+  -- bgImage = "",
+  roundCorner = true,
+  windowControls = true,
+  lineNumber = true,
+  font = "Source Code Pro",
+  lineOffset = 1, -- from where to start line number
+  linePad = 2,   -- padding between lines
+  padHoriz = 80, -- Horizontal padding
+  padVert = 100, -- vertical padding
+  shadowBlurRadius = 10,
+  shadowColor = "#555555",
+  shadowOffsetX = 8,
+  shadowOffsetY = 8,
+  gobble = false, -- enable lsautogobble like feature
+  debug = false, -- enable debug output
+})
 
