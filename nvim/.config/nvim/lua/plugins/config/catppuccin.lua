a1a6e6fc4144c16265b2465f3fe0d715f962d510
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-01-27 15:09:43 -0800

    remove global vars. conflicts with terminal conflig

--- nvim/.config/nvim/lua/plugins/config/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/config/catppuccin.lua
@@ -6,39 +6,6 @@ if not status_ok then
 end
 
 
--- flavours include dusk, latte, frappe, macchiato, mocha
-local iterm_profile = os.getenv('ITERM_PROFILE')
-
-if (iterm_profile == nil) then
-  -- if not using iterm, just setup catpuccin but don't define flavor yet
-  return
-end
-
-if (string.match(iterm_profile, 'Catppuccin')) then
-
-  if (string.match(iterm_profile, 'Frappe')) then
-    vim.g.catppuccin_flavour = "frappe"
-  end
-
-  if (string.match(iterm_profile, 'Latte')) then
-    vim.g.catppuccin_flavour = "latte"
-  end
-  
-  if (string.match(iterm_profile, 'Macchiato')) then
-    vim.g.catppuccin_flavour = "macchiato"
-  end
-  
-  if (string.match(iterm_profile, 'Mocha')) then
-    vim.g.catppuccin_flavour = "mocha"
-  end
-  
-else
-
-  vim.g.catppuccin_flavour = "mocha"
-  
-end
-
-
 catppuccin.setup({
   dim_inactive = {
     enabled = false,
