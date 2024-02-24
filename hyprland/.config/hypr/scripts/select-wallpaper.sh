218bf0fcc85fe7fe2f513083f3cb29fa393547d1
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-02-24 00:33:12 -0800

    dmenu should list theme name, not file name
    previously the dmenu was listing the wallpaper image name
    using json to describe themes in a richer, more detailed manner

--- hyprland/.config/hypr/scripts/select-wallpaper.sh
+++ hyprland/.config/hypr/scripts/select-wallpaper.sh
@@ -4,7 +4,8 @@
 DEFAULT_THEME="embark"
 WALLPAPER_CACHE=~/.cache/wallpaper
 DMENU="rofi -dmenu -i"
-WALLPAPER_MAPPING=~/.config/hypr/consts/prof-mapping.txt
+WALLPAPERS_DIR=~/wallpapers
+WALLPAPERS_JSON=~/wallpapers/_wallpapers.json
 
 check_cache() {
   if [ ! -d $WALLPAPER_CACHE ]; then
@@ -28,7 +29,7 @@ update_cache() {
   first_char=$(echo $1 | cut -b 1)
   if [[ "${first_char}" != "#" ]]; then
     echo "updating cache with image $1"
-    cp ~/.config/hypr/wallpapers/$1 ~/.cache/wallpaper/$1
+    cp $WALLPAPERS_DIR/$1 $WALLPAPER_CACHE/$1
   fi
 }
 
@@ -53,43 +54,60 @@ get_rofi_theme() {
   echo $rasi_file
 }
 
-select_wallpaper() {
-  # image.png,blue --> image.png
-  local wallpaper=$(cat $WALLPAPER_MAPPING | awk -F "," '{print $1}' | $DMENU -p "Themes" -config "$(get_rofi_theme)")
-  echo $wallpaper
-}
 
-select_theme() {
-  # image.png,blue --> blue
-  local prof_theme=$(cat $WALLPAPER_MAPPING | awk -F "," '{print $2}' | $DMENU -p "Themes" -config "$(get_rofi_theme)")
-  echo $prof_theme
+# this gives a little csv list describing the theme
+select_desktop_theme() {
+  local theme_key=$(jq -r 'keys | join("\n")' $WALLPAPERS_JSON | $DMENU -p "Themes" -config "$(get_rofi_theme)")
+
+  if [ -z "$theme_key" ]; then
+    echo "cancel"
+    exit 1
+  fi
+
+  local payload=$(jq -r --arg k "$theme_key" '.[$k] | to_entries | map("\(.key),\(.value)") | .[]' $WALLPAPERS_JSON)
+  echo -e "theme,$theme_key\n$payload"
 }
 
 
 update_wallpaper() {
   check_cache
-  local selected_wallpaper="$(select_wallpaper)"
 
-  # if you did not select a wallpaper exit early
-  if [ -z "$selected_wallpaper" ]; then
-    echo "did not select a wallpaper"
+  desktop_theme=$(select_desktop_theme)
+
+  # if you did not select a theme exit early
+  echo "desktop theme : $desktop_theme"
+  if [ "$desktop_theme" == "cancel" ]; then
+    echo "did not select a desktop theme"
     exit 1
   fi
+  
+  theme=$(echo "$desktop_theme" | grep "^theme" | cut -d ',' -f 2)
+  kitty_theme=$(echo "$desktop_theme" | grep "^kitty" | cut -d ',' -f 2)
+  wallpaper=$(echo "$desktop_theme" | grep "^wallpaper" | cut -d ',' -f 2)
+  background=$(echo "$desktop_theme" | grep "^bg_color" | cut -d ',' -f 2)
 
-  local derived_theme="$(echo $selected_wallpaper | grep -f - $WALLPAPER_MAPPING | awk -F ',' '{print $2}')"
+  # put this info into an associative array
+  declare -A theme_arr
+  while IFS=, read -r key value; do
+    theme_arr["$key"]="$value"
+  done <<< $desktop_theme
 
-  update_cache $selected_wallpaper $derived_theme
-
-  first_char=$(echo $selected_wallpaper | cut -b 1)
-  if [[ "${first_char}" == "#" ]]; then
-    echo "using hexcode $selected_wallpaper"
-    swww clear $selected_wallpaper
+  # call swww based on whether we're using an image or flat background color
+  if [[ -v theme_arr["wallpaper"] ]]; then
+    # use swwww setting the wallpaper
+    echo "this is a wallpaper theme"
+    echo "using image $wallpaper"
+    swww img -t wipe --transition-step 190 --transition-duration 1.3 ~/wallpapers/$wallpaper
+    update_cache $wallpaper $kitty_theme
   else
-    echo "using image $selected_wallpaper"
-    swww img -t wipe --transition-step 190 --transition-duration 1.3 ~/.config/hypr/wallpapers/$selected_wallpaper
+    # use swwww setting a flat background image
+    echo "this is a flat wallpaper theme"
+    echo "using color $background"
+    swww clear $background
+    update_cache $background $kitty_theme
   fi
 
-  kitty +kitten themes --dump-theme $derived_theme > ~/.config/kitty/tmp/current-theme.conf
+  kitty +kitten themes --dump-theme $kitty_theme > ~/.config/kitty/tmp/current-theme.conf
   local restart_processes=("kitty" "nvim")
   for p in "${restart_processes[@]}"; do
     echo "checking process : $p"
@@ -101,7 +119,7 @@ update_wallpaper() {
   done
 
   # update waybar style
-  cp ~/.config/waybar/themes/$derived_theme.css ~/.config/waybar/tmp/style.css
+  cp ~/.config/waybar/themes/$kitty_theme.css ~/.config/waybar/tmp/style.css
   pkill -x waybar
   waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/tmp/style.css &
 
