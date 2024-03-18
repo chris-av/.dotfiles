e2babff7019fe6d35085d2f458f97cc49a07ea70
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-03-18 00:34:55 -0700

    remove trailing spaces

--- nvim/.config/nvim/lua/lsp/html.lua
+++ nvim/.config/nvim/lua/lsp/html.lua
@@ -1,5 +1,5 @@
-local on_attach = require('lsp.on_attach') 
-local nvim_lsp = require('lspconfig') 
+local on_attach = require('lsp.on_attach')
+local nvim_lsp = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
