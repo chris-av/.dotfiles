3f7355572177cf1103d399aee947325f5499670d
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-03-18 20:02:02 -0700

    add config

---
+++ tmux/.config/tmux/utils/themes/catppuccin-latte.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+pane_active_border_fg="#ebdbb2"
+pane_border_fg="#665c54"
+message_style_fg="#ebdbb2"
+message_style_bg="#282828"
+status_style_fg="#ebdbb2"
+status_style_bg="#282828"
+status_x_fg="#ebdbb2"
+status_x_bg="#282828"
+status_y_fg="#ebdbb2"
+status_y_bg="#282828"
+status_z_fg="#ebdbb2"
+status_z_bg="#282828"
+flags_fg="#000000"
+
+# none text before separator
+window_x_fg="#282828"
+window_x_bg="#ebdbb2"
+
+# other windows
+window_y_fg="#282828"
+window_y_bg="#ebdbb2"
+
+# flag symbols
+window_z_bg="#282828"
+window_z_fg="#ebdbb2"
+
+window_fmt_fg="#ebdbb2"
+window_fmt_bg="#282828"
+
+client_prefix="#3c3836"
+
+
---
+++ tmux/.config/tmux/utils/themes/matrix.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# foreground            #00e82f
+# background            #000000
+
+pane_active_border_fg="#00e82f"
+pane_border_fg="#00e82f"
+message_style_fg="#00e82f"
+message_style_bg="#000000"
+status_style_fg="#00e82f"
+status_style_bg="#000000"
+status_x_fg="#00e82f"
+status_x_bg="#000000"
+status_y_fg="#00e82f"
+status_y_bg="#000000"
+status_z_fg="#00e82f"
+status_z_bg="#000000"
+flags_fg="#00e82f"
+
+# none text before separator
+window_x_fg="#000000"
+window_x_bg="#000000"
+
+# other windows
+window_y_fg="#000000"
+window_y_bg="#00e82f"
+
+# flag symbols
+window_z_bg="#00e82f"
+window_z_fg="#000000"
+
+window_fmt_fg="#00e82f"
+window_fmt_bg="#000000"
+
+client_prefix="#3c3836"
+
