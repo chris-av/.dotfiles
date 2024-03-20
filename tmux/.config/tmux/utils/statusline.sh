a7d4f32ed867b71e41bbf51fd91a1f17a6fd6cf3
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-03-19 19:36:59 -0700

    move config to individual files, since some might be different

--- tmux/.config/tmux/utils/statusline.sh
+++ tmux/.config/tmux/utils/statusline.sh
@@ -12,39 +12,3 @@ elif [[ $TERM_PROFILE == "Matrix" || $ITERM_PROFILE == "Matrix" ]]; then
 fi
 
 
-# set everything else
-left_sep=""
-right_sep=""
-
-# set length
-tmux set-option -g status-left-length 100
-tmux set-option -g status-right-length 100
-
-# pane border styling
-tmux set-option -g pane-active-border-style "fg=${pane_active_border_fg}"
-tmux set-option -g pane-border-style "fg=${pane_border_fg}"
-
-# message styling
-tmux set-option -g message-style "fg=${message_style_fg},bg=${message_style_bg}"
-
-# status bar
-tmux set-option -g status-style "fg=${status_style_fg},bg=${status_style_bg}"
-
-# Status left
-tmux set-option -g status-left "#[bg=${status_x_bg},fg=${status_x_fg}]#{?client_prefix,#[bg=${client_prefix}],} #S "
-
-# Status right
-tmux set-option -g status-right ""
-
-
-# Window option
-flags="#{?window_flags,#[fg=${flags_fg}]#{window_flags},}"
-current_flags="#{?window_flags,#[fg=${flags_fg}]#{window_flags},}"
-
-tmux set-window-option -g window-status-current-format "#[fg=${window_x_fg},bg=${window_x_bg}]${left_sep}#[fg=${window_y_fg},bg=${window_y_bg}] #I #W${current_flags} #[fg=${window_z_fg},bg=${window_z_bg}]${left_sep}"
-tmux set-window-option -g window-status-format "#[fg=${window_fmt_fg}]#[bg=${window_fmt_bg}] #I #W${flags}"
-tmux set-window-option -g window-status-activity-style "bold"
-tmux set-window-option -g window-status-bell-style "bold"
-
-
-
