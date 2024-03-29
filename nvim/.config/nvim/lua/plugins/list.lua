7e69802b89d7d0191b80f47e3027d8db9e1913ad
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-03-29 00:57:43 -0700

    update command

--- nvim/.config/nvim/lua/plugins/list.lua
+++ nvim/.config/nvim/lua/plugins/list.lua
@@ -13,7 +13,7 @@ local plugins = {
   {
     'laytan/tailwind-sorter.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
-    build = 'cd formatter && npm i && npm run build',
+    build = 'cd formatter && npm ci && npm run build',
     config = true,
   },
 
