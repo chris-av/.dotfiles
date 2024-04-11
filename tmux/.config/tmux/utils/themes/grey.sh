4ee5419427fcfba5173778663d2b47a05260f8b6
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-04-10 21:14:44 -0700

    create custom theme for grey

---
+++ tmux/.config/tmux/utils/themes/grey.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+pane_active_border_fg="#cccccc"
+pane_border_fg="#cccccc"
+message_style_fg="#cccccc"
+message_style_bg="#000000"
+status_style_fg="#cccccc"
+status_style_bg="#000000"
+status_x_fg="#cccccc"
+status_x_bg="#000000"
+status_y_fg="#cccccc"
+status_y_bg="#000000"
+status_z_fg="#cccccc"
+status_z_bg="#000000"
+flags_fg="#cccccc"
+
+# none text before separator
+window_x_fg="#000000"
+window_x_bg="#cccccc"
+
+# other windows
+window_y_fg="#000000"
+window_y_bg="#cccccc"
+
+# flag symbols
+window_z_bg="#cccccc"
+window_z_fg="#000000"
+
+window_fmt_fg="#cccccc"
+window_fmt_bg="#000000"
+
+client_prefix_bg="#cccccc"
+client_prefix_fg="#000000"
+
+
+# left_sep=""
+# right_sep=""
+left_sep=""
+right_sep=""
+
+
