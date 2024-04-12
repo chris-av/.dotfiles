7e9efc28bd6e0f9d68ca385b58ef9fd895dea648
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-04-12 01:28:13 -0700

    add configs for telescope

--- nvim/.config/nvim/lua/plugins/config/telescope.lua
+++ nvim/.config/nvim/lua/plugins/config/telescope.lua
@@ -1,5 +1,5 @@
 local status_ok, telescope = pcall(require, 'telescope')
-local notifications = require('utils.notifications') 
+local notifications = require('utils.notifications')
 if not status_ok then
   notifications.plugin_error('telescope')
   return
@@ -14,8 +14,24 @@ telescope.setup({
     mappings = {
       i = {
         ["<Esc>"] = actions.close,
-      },
-    }
-  }
-})
 
+        -- git stuff
+        ["<C-s>"] = actions.cycle_previewers_next,
+        ["<C-a>"] = actions.cycle_previewers_prev,
+
+      },
+    },
+    preview = {
+      filesize_limit = 0.5, -- MB
+    },
+  },
+  pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+        },
+      },
+    },
+  },
+})
