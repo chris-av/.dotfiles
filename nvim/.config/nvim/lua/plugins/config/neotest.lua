cb1cfe2004dd9f0f94d3f9745a2c487fb2011fa9
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-05-04 17:29:33 -0700

    BUGFIX: hardcode jest command
    the getJestCommand util function bricks the nvim process on any
    project that's not javascript. hardcoding the command for now.

--- nvim/.config/nvim/lua/plugins/config/neotest.lua
+++ nvim/.config/nvim/lua/plugins/config/neotest.lua
@@ -1,7 +1,12 @@
 require('neotest').setup({
   adapters = {
+    -- require('neotest-jest')({
+    --   jest_test_discovery = false,
+    --   jestCommand = require('neotest-jest.jest-util').getJestCommand(vim.fn.expand '%:p:h') .. ' --watch',
+    -- }),
     require('neotest-jest')({
-      jestCommand = require('neotest-jest.jest-util').getJestCommand(vim.fn.expand '%:p:h') .. ' --watch',
+      jest_test_discovery = false,
+      jestCommand = 'jest --watch',
     }),
   },
 })
