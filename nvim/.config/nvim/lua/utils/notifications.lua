51e3fa3deac535d27b77bf849eaaac0ddbdb576e
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-05-04 17:56:53 -0700

    pass enum instead of string

--- nvim/.config/nvim/lua/utils/notifications.lua
+++ nvim/.config/nvim/lua/utils/notifications.lua
@@ -1,16 +1,17 @@
 local M = {}
+local level = vim.log.levels
 
 
-M.plugin_error = function(plugin_name) 
-  vim.notify('could not load ' .. plugin_name, 'error', {
+M.plugin_error = function(plugin_name)
+  vim.notify('could not load ' .. plugin_name, level.ERROR, {
     title = 'Plugin Error'
   })
 end
 
-M.log = function(title, message) 
+M.log = function(title, message)
   -- can be debug, error, info, trace, or warn
   -- trace does not show dialog popup
-  vim.notify(message, 'info', {
+  vim.notify(message, level.INFO, {
     title = title
   })
 end
