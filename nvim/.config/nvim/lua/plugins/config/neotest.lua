ec1a4485d57354ab32d84d877c29afb2f5210171
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-05-05 22:37:19 -0700

    refactor

--- nvim/.config/nvim/lua/plugins/config/neotest.lua
+++ nvim/.config/nvim/lua/plugins/config/neotest.lua
@@ -1,11 +1,8 @@
 require('neotest').setup({
   adapters = {
-    -- require('neotest-jest')({
-    --   jest_test_discovery = false,
-    --   jestCommand = require('neotest-jest.jest-util').getJestCommand(vim.fn.expand '%:p:h') .. ' --watch',
-    -- }),
     require('neotest-jest')({
       jest_test_discovery = false,
+      -- jestCommand = require('neotest-jest.jest-util').getJestCommand(vim.fn.expand '%:p:h') .. ' --watch',
       jestCommand = 'jest --watch',
     }),
     require("neotest-python"),
