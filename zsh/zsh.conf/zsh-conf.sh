7b9d3577d043b51a1722cd9b56a2dbe8cb422bdf
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-08-24 22:38:33 -0400

    simplify comments

--- zsh/zsh.conf/zsh-conf.sh
+++ zsh/zsh.conf/zsh-conf.sh
@@ -1,12 +1,5 @@
-# Which plugins would you like to load?
-# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
 plugins=(
-
   git
-# zsh-autosuggestions
   zsh-syntax-highlighting
 )
 
@@ -17,15 +10,16 @@ ZSH_HIGHLIGHT_STYLES[path]=none
 ZSH_HIGHLIGHT_STYLES[path_prefix]=none
 
 
-
-# load in the colors!
+# load in the colors in accessible array format!
 autoload -U colors && colors
 
 # get git and branch info
 autoload -Uz vcs_info
 precmd() { vcs_info }
 
+# set zstyle
 zstyle ':vcs_info:git:*' formats 'on branch %b '
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 # set the z shell prompt
 PROMPT='%n%{$reset_color%} in %{$fg[yellow]%}%c %{$reset_color%}% %{$fg[blue]%}${vcs_info_msg_0_}%{$reset_color%}> '
@@ -33,6 +27,5 @@ PROMPT='%n%{$reset_color%} in %{$fg[yellow]%}%c %{$reset_color%}% %{$fg[blue]%}$
 # set the colors of terminal output
 LS_COLORS='no=00;37:fi=00:di=01;34:ln=00;36:pi=01;33:so=01;35:bd=40;33;01:'
 export LS_COLORS
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 eval "$(fzf --zsh)"
