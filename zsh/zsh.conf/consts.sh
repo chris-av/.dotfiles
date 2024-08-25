22d96b41990692caa91b8bd315da226f65ab53c8
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-08-24 22:38:03 -0400

    FZF configs

--- zsh/zsh.conf/consts.sh
+++ zsh/zsh.conf/consts.sh
@@ -65,3 +65,7 @@ if [ -d ~/.spicetify ]; then
   export PATH=$PATH:~/.spicetify
 fi
 
+# FZF configs
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
--- zsh/zsh.conf/zsh-conf.sh
+++ zsh/zsh.conf/zsh-conf.sh
@@ -34,3 +34,5 @@ PROMPT='%n%{$reset_color%} in %{$fg[yellow]%}%c %{$reset_color%}% %{$fg[blue]%}$
 LS_COLORS='no=00;37:fi=00:di=01;34:ln=00;36:pi=01;33:so=01;35:bd=40;33;01:'
 export LS_COLORS
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+eval "$(fzf --zsh)"
