1c9b781cda1e86b3295a5d2c271d740fa8caa0f8
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-09-23 00:12:44 -0400

    better R config

--- nvim/.config/nvim/lua/plugins/config/rnvim.lua
+++ nvim/.config/nvim/lua/plugins/config/rnvim.lua
@@ -6,23 +6,28 @@ local opts = {
       -- of files supported by R.nvim. This is an
       -- opportunity to create mappings local to buffers.
       if vim.o.syntax ~= "rbrowser" then
-        vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
-        vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+        vim.keymap.set("n", "<Enter>", "<Plug>RDSendLine", { desc = "R - Send current line to console" })
+        vim.keymap.set("v", "<Enter>", "<Plug>RSendSelection", { desc = "R - Send current selection to console" })
+        vim.keymap.set("n", "<leader>p", "<Plug>RViewDF", { desc = "R - open dataframe under cursor with dv" })
       end
     end
   },
-  min_editor_width = 72,
-  rconsole_width = 90,
+  -- min_editor_width = 72,
+  rconsole_width = 120,
   disable_cmds = {
     "RClearConsole",
     "RCustomStart",
     "RSPlot",
     "RSaveClose",
   },
-  auto_start = "no",
+  -- auto_start = "on startup", -- no
+  auto_start = "no", -- no
   objbr_auto_start = true,
   objbr_place = "BOTTOM",
-  rconsole_with = 40,
+  csv_app = "terminal:vd",
+  pipe_keymap = "<M-P>",
+  pipe_version = "magrittr",
+  assignment_keymap = "<M-=>",
 }
 
 -- Check if the environment variable "R_AUTO_START" exists.
@@ -34,4 +39,3 @@ if vim.env.R_AUTO_START == "true" then
 end
 
 require("r").setup(opts)
-
