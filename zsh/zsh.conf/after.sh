906d5dd020091ec7a7f4098aeb282863c392b2d9
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-11-15 12:41:17 -0500

    use pokeget in fastfetch

--- zsh/zsh.conf/after.sh
+++ zsh/zsh.conf/after.sh
@@ -4,3 +4,9 @@ source ~/.aliases
 for file in ~/functions/*; do
   source $file
 done
+
+echo ""
+echo ""
+if [ ! command -v pokeget > /dev/null 2>&1 || ! command -v fastfetch > /dev/null 2>&1 ]; then
+  pokeget random --hide-name | fastfetch --file-raw -
+fi
