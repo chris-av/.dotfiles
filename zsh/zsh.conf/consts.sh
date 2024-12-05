fe10828335e3fb1d298fcbb06d77dc893b1da9c9
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-12-05 15:34:17 -0800

    add pipx path

--- zsh/zsh.conf/consts.sh
+++ zsh/zsh.conf/consts.sh
@@ -70,3 +70,6 @@ fi
 export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+
+# pipx
+export PATH=$PATH:/home/chrisv/.local/bin
