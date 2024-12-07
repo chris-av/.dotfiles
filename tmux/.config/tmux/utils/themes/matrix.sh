7ad9d8cd59fd6f13efed15d9019f27413a50b7a1
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-12-06 22:38:04 -0800

    better green for matrix theme

--- tmux/.config/tmux/utils/themes/matrix.sh
+++ tmux/.config/tmux/utils/themes/matrix.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bash
-pane_active_border_fg="#00e82f"
-pane_border_fg="#00e82f"
-message_style_fg="#00e82f"
+pane_active_border_fg="#90ee90"
+pane_border_fg="#90ee90"
+message_style_fg="#90ee90"
 message_style_bg="#000000"
-status_style_fg="#00e82f"
+status_style_fg="#90ee90"
 status_style_bg="#000000"
-status_x_fg="#00e82f"
+status_x_fg="#90ee90"
 status_x_bg="#000000"
-status_y_fg="#00e82f"
+status_y_fg="#90ee90"
 status_y_bg="#000000"
-status_z_fg="#00e82f"
+status_z_fg="#90ee90"
 status_z_bg="#000000"
-flags_fg="#00e82f"
+flags_fg="#90ee90"
 
 # none text before separator
 window_x_fg="#000000"
-window_x_bg="#00e82f"
+window_x_bg="#90ee90"
 
 # other windows
 window_y_fg="#000000"
-window_y_bg="#00e82f"
+window_y_bg="#90ee90"
 
 # flag symbols
-window_z_bg="#00e82f"
+window_z_bg="#90ee90"
 window_z_fg="#000000"
 
-window_fmt_fg="#00e82f"
+window_fmt_fg="#90ee90"
 window_fmt_bg="#000000"
 
-client_prefix_bg="#00e82f"
+client_prefix_bg="#90ee90"
 client_prefix_fg="#000000"
 
 
@@ -37,3 +37,5 @@ client_prefix_fg="#000000"
 left_sep=""
 right_sep=""
 
+window_sel_fg="#000000"
+window_sel_bg="#90ee90"
