3df030303d2d05b4c1d647d47961ea3d10f2f14e
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-12-12 18:14:54 -0800

    tmux style for catppuccin mocha

---
+++ tmux/.config/tmux/utils/themes/catppuccin-mocha.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+pane_active_border_fg="#A6E3A1"
+pane_border_fg="#CDD6F4"
+message_style_fg="#CDD6F4"
+message_style_bg="#1E1E2E"
+status_style_fg="#CDD6F4"
+status_style_bg="#1E1E2E"
+status_x_fg="#1E1E2E"
+status_x_bg="#F38BA8"
+status_y_fg="#1E1E2E"
+status_y_bg="#F5C2E7"
+status_z_fg="#1E1E2E"
+status_z_bg="#F5C2E7"
+flags_fg="#45475A"
+
+# none text before separator
+window_x_fg="#45475A"
+window_x_bg="#A6E3A1"
+
+# other windows
+window_y_fg="#45475A"
+window_y_bg="#A6E3A1"
+
+# flag symbols
+window_z_bg=""
+window_z_fg=""
+
+window_fmt_fg="#CDD6F4"
+window_fmt_bg="#1E1E2E"
+
+client_prefix_bg="#CDD6F4"
+client_prefix_fg="#1E1E2E"
+
+
+# left_sep=""
+# right_sep=""
+left_sep=""
+right_sep=""
+
+window_sel_fg="#45475A"
+window_sel_bg="#A6E3A1"
