2333743bc323c0e2712f7bf0e88d7379516a7214
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-01-24 21:00:50 -0800

    use pound sign prompt

--- zsh/zsh.conf/zsh-conf.sh
+++ zsh/zsh.conf/zsh-conf.sh
@@ -23,7 +23,7 @@ zstyle ':vcs_info:git:*' formats ' %b '
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 # set the z shell prompt
-PROMPT='%n%{$reset_color%} in %{$fg[yellow]%}%c %{$reset_color%}% %{$fg[blue]%}${vcs_info_msg_0_}%{$reset_color%}> '
+PROMPT='%n%{$reset_color%} in %{$fg[yellow]%}%c %{$reset_color%}% %{$fg[blue]%}${vcs_info_msg_0_}%{$reset_color%}# '
 
 # set the colors of terminal output
 LS_COLORS='no=00;37:fi=00:di=01;34:ln=00;36:pi=01;33:so=01;35:bd=40;33;01:'
