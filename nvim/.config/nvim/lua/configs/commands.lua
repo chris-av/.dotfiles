5ab17d6225227ee22414ec6e4262dfa0207e3319
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-02-08 21:49:36 -0800

    remove these templates, use luasnip

--- nvim/.config/nvim/lua/configs/commands.lua
+++ nvim/.config/nvim/lua/configs/commands.lua
@@ -16,25 +16,7 @@ vim.api.nvim_create_user_command(
 )
 
 
-local group = vim.api.nvim_create_augroup('skeleton', { clear = true })
-vim.api.nvim_create_autocmd('BufNewFile', {
-  pattern = { 'index.html' },
-  command = "silent! execute '0r ~/.config/nvim/templates/index.'.expand('<afile>:e')",
-  group = group
-})
-
-vim.api.nvim_create_autocmd('BufNewFile', {
-  pattern = { '*.sh' },
-  command = "silent! execute '0r ~/.config/nvim/templates/script.'.expand('<afile>:e') | 3",
-  group = group
-})
-
-vim.api.nvim_create_autocmd('BufNewFile', {
-  pattern = { 'jsconfig.json' },
-  command = "silent! execute '0r ~/.config/nvim/templates/jsconfig.'.expand('<afile>:e')",
-  group = group
-})
-
+local group = vim.api.nvim_create_augroup('term', { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = {},
   command = "setlocal listchars = nonumber norelativenumber",
