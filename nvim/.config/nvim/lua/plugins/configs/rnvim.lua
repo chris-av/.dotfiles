4511540c3cc2485b22c61d948c60908a4026bfa0
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-02-08 23:15:06 -0800

    refactor rnvim config

--- nvim/.config/nvim/lua/plugins/configs/rnvim.lua
+++ nvim/.config/nvim/lua/plugins/configs/rnvim.lua
@@ -1,4 +1,4 @@
-local opts = {
+require("r").setup({
   R_args = { "--quiet", "--no-save" },
   hook = {
     after_config = function()
@@ -20,22 +20,14 @@ local opts = {
     "RSPlot",
     "RSaveClose",
   },
-  -- auto_start = "on startup", -- no
-  auto_start = "no", -- no
+  auto_start = vim.env.R_AUTOSTART and "on startup" or "no", -- no
   objbr_auto_start = true,
   objbr_place = "BOTTOM",
-  csv_app = "terminal:vd",
   pipe_keymap = "<M-P>",
   pipe_version = "magrittr",
   assignment_keymap = "<M-=>",
-}
-
--- Check if the environment variable "R_AUTO_START" exists.
--- If using fish shell, you could put in your config.fish:
--- alias r "R_AUTO_START=true nvim"
-if vim.env.R_AUTO_START == "true" then
-  opts.auto_start = 1
-  opts.objbr_auto_start = true
-end
-
-require("r").setup(opts)
+  view_df = {
+    open_app = "tmux new-window vd",
+    how = "tabnew",
+  },
+})
