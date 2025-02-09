a6aef0f4f6154ec520faaa93b6dda095609aaca8
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-02-08 22:07:24 -0800

    remove trailing spaces, weird newlines

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,4 +6,3 @@ require('debuggers')
 -- set colorscheme
 local colorutils = require('utils/colorutils')
 colorutils.set_colorscheme()
-
--- nvim/.config/nvim/lua/lsp/r-lsp.lua
+++ nvim/.config/nvim/lua/lsp/r-lsp.lua
@@ -1,4 +1,4 @@
-local on_attach = require('lsp.on_attach') 
+local on_attach = require('lsp.on_attach')
 local nvim_lsp = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
