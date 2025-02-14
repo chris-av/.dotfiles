9a6fa59f2b07b09ffabf4d396e00c705335d5637
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-02-13 20:58:09 -0800

    scaffold fluoromachine theming

--- nvim/.config/nvim/lua/utils/colorutils.lua
+++ nvim/.config/nvim/lua/utils/colorutils.lua
@@ -18,6 +18,7 @@ M.theme_table = {
   ["snazzy"] = { colorscheme = "snazzy", lualine_theme = std_path .. "material" },
   ["evil"] = { colorscheme = "evil", lualine_theme = "auto" },
   ["rose-pine"] = { colorscheme = "rose-pine-main", lualine_theme = "auto" },
+  ["fluoromachine"] = { colorscheme = "fluoromachine", lualine_theme = std_path .. "fluoromachine", },
 }
 
 -- set colorscheme based on environment variable
