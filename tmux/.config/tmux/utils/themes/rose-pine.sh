3e029b34d360761db31409477f36784ccfa115f0
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-03-28 23:20:24 -0700

    update tmux style

--- tmux/.config/tmux/utils/themes/rose-pine.sh
+++ tmux/.config/tmux/utils/themes/rose-pine.sh
@@ -5,8 +5,8 @@ message_style_fg="#e0def4"
 message_style_bg="#191724"
 status_style_fg="#e0def4"
 status_style_bg="#191724"
-status_x_fg="#000000"
-status_x_bg="#eb6f92"
+status_x_fg="#e0def4"
+status_x_bg="#191724"
 status_y_fg="#e0def4"
 status_y_bg="#191724"
 status_z_fg="#e0def4"
@@ -14,8 +14,8 @@ status_z_bg="#191724"
 flags_fg="#31748f"
 
 # none text before separator
-window_x_fg="#e0def4"
-window_x_bg="#c4a7e7"
+window_x_fg="#191724"
+window_x_bg="#c5a7e7"
 
 # other windows
 window_y_fg="#191724"
@@ -23,7 +23,7 @@ window_y_bg="#c4a7e7"
 
 # flag symbols
 window_z_bg="#000000"
-window_z_fg="#000000"
+window_z_fg="#c4a7e8"
 
 window_fmt_fg="#9ccfd8"
 window_fmt_bg="#191724"
@@ -31,11 +31,8 @@ window_fmt_bg="#191724"
 client_prefix_bg="#f6c177"
 client_prefix_fg="#000000"
 
-
-# left_sep=""
-# right_sep=""
-left_sep=""
-right_sep=""
+left_sep=""
+right_sep=""
 
 window_sel_fg="#e0def4"
 window_sel_bg="#403d52"
