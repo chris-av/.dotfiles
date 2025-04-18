3bc3a0041fbeb08039e6628275744dfa62a5ac2a
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-04-18 14:38:07 -0700

    scaffold installation that takes options

--- utilities/linux-installation.sh
+++ utilities/linux-installation.sh
@@ -24,7 +24,75 @@ else
 fi
 
 
-# install all the dependencies whose "repository" is not null
-yq -r \
-  '.packages | map(select(.repository != null)) | map(.name) | .[]' ~/.dotfiles/packages/list.yml | \
-  xargs yay -S
+PKD_DIR=~/.dotfiles/packages
+echo $PKD_DIR
+
+MODE=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --all)
+      MODE="all"
+      shift
+      ;;
+    --extra)
+      MODE="extra"
+      shift
+      ;;
+    --aur)
+      MODE="aur"
+      shift
+      ;;
+    *)
+      echo "Invalid argument: $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z $MODE ]; then
+
+  echo "Select the packages to install : "
+
+  select option in "all" "extra" "aur"; do
+    case $option in 
+      all)
+        MODE="all"
+        break
+        ;;
+      extra)
+        MODE="extra"
+        break
+        ;;
+      aur)
+        MODE="aur"
+        break
+        ;;
+      *)
+        echo "Invalid option, please try again"
+        ;;
+    esac
+  done
+
+fi
+
+
+case $MODE in 
+  all)
+    FILTER_CMD='.[] | .name'
+    break
+    ;;
+  extra)
+    FILTER_CMD='.[] | select(.repository == "extra") | .name'
+    break
+    ;;
+  aur)
+    FILTER_CMD='.[] | select(.repository == "aur") | .name'
+    break
+    ;;
+  *)
+    echo "Invalid option, please try again"
+    ;;
+esac
+
+find $PKD_DIR -type f -name "*.yml" | xargs cat | yq -Mr "$FILTER_CMD" -
