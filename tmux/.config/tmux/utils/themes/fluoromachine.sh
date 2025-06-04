14e98cdfe5d682d84e0009b0c832ed592022d7b0
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-06-03 18:57:56 -0700

    update tmux theme

--- tmux/.config/tmux/utils/themes/fluoromachine.sh
+++ tmux/.config/tmux/utils/themes/fluoromachine.sh
@@ -1,41 +1,39 @@
 #!/usr/bin/env bash
 pane_active_border_fg="#72F1B8"
 pane_border_fg="#524f67"
-message_style_fg="#F8F8F8"
+message_style_fg="#72F1B8"
 message_style_bg="#200933"
 status_style_fg="#F8F8F8"
 status_style_bg="#200933"
-status_x_fg="#241B2F"
-status_x_bg="#BF8AFA"
-status_y_fg="#F8F8F8"
+status_x_fg="#72F1B8"
+status_x_bg="#200933"
+status_y_fg="#72F1B8"
 status_y_bg="#200933"
-status_z_fg="#F8F8F8"
+status_z_fg="#72F1B8"
 status_z_bg="#200933"
 flags_fg="#241B2F"
 
 # none text before separator
-window_x_fg="#F8F8F8"
+window_x_fg="#200933"
 window_x_bg="#72F1B8"
 
 # other windows
-window_y_fg="#000000"
+window_y_fg="#200933"
 window_y_bg="#72F1B8"
 
 # flag symbols
-window_z_bg="#000000"
-window_z_fg="#000000"
+window_z_bg="#200933"
+window_z_fg="#72F1B8"
 
 window_fmt_fg="#F8F8F8"
 window_fmt_bg="#200933"
 
-client_prefix_bg="#72F1B9"
-client_prefix_fg="#000000"
+# client_prefix_bg="#72F1B9"
+# client_prefix_fg="#000000"
 
 
-# left_sep=""
-# right_sep=""
-left_sep=""
-right_sep=""
+left_sep=""
+right_sep=""
 
 window_sel_fg="#495495"
 window_sel_bg="#72F1B8"
