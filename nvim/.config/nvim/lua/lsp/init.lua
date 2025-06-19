c48b61ac3b6bbe3d7adb480664aca9efb64ae4de
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-06-18 23:36:21 -0700

    combine mason and lsp config together for testing

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -1,3 +1,25 @@
+require('mason').setup()
+require('mason-lspconfig').setup({
+  automatic_installation = true,
+  ensure_installed = {
+    "bashls",
+    "clangd",
+    "cssls",
+    "gopls",
+    "html",
+    "jdtls",
+    "jsonls",
+    "ltex",
+    "lua_ls",
+    "pyright",
+    "r_language_server",
+    "rust_analyzer",
+    "tailwindcss",
+    "ts_ls",
+    "zls",
+  },
+})
+
 local on_attach = require("utils.on_attach")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
--- nvim/.config/nvim/lua/plugins/configs/mason.lua
+++ nvim/.config/nvim/lua/plugins/configs/mason.lua
@@ -3,27 +3,4 @@ return {
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
   },
-  config = function()
-    require('mason').setup()
-    require('mason-lspconfig').setup({
-      automatic_installation = true,
-      ensure_installed = {
-        "bashls",
-        "clangd",
-        "cssls",
-        "gopls",
-        "html",
-        "jdtls",
-        "jsonls",
-        "ltex",
-        "lua_ls",
-        "pyright",
-        "r_language_server",
-        "rust_analyzer",
-        "tailwindcss",
-        "ts_ls",
-        "zls",
-      },
-    })
-  end
 }
