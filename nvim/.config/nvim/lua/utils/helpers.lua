399022c1b07615b373cd8547c03b626b946d5781
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-06-18 23:28:41 -0700

    colocate lsp related fns to keymaps in on_attach

--- nvim/.config/nvim/lua/utils/helpers.lua
+++ nvim/.config/nvim/lua/utils/helpers.lua
@@ -38,41 +38,6 @@ M.prepare_pyenv = function()
   end
 end
 
-M.ls_ws_folders = function()
-  print(vim.inspect(buf.list_workspace_folders()))
-end
-
-M.custom_hover = function()
-  local hover = buf.hover
-  local winid = require('ufo').peekFoldedLinesUnderCursor()
-  if not winid then
-    hover()
-  end
-end
-
-M.open_diagnostic = function()
-  vim.diagnostic.open_float(0, { border = "single" })
-end
-
-M.prev_diagnostic = function()
-  vim.diagnostic.goto_prev({ float = { border = "single" } })
-end
-
-M.next_diagnostic = function()
-  vim.diagnostic.goto_next({ float = { border = "single" } })
-end
-
-M.format_file = function()
-  vim.lsp.buf.format({ async = true, })
-end
-
-M.manage_loclist = function()
-  vim.diagnostic.setloclist({ open = false }) -- don't open and focus
-  local window = vim.api.nvim_get_current_win()
-  vim.cmd.lwindow()                           -- open+focus loclist if has entries, else close -- this is the magic toggle command
-  vim.api.nvim_set_current_win(window)        -- restore focus to window you were editing (delete this if you want to stay in loclist)
-end
-
 M.toggle_cond_breakpoint = function()
   -- dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
   local condition = vim.fn.input({ prompt = 'Breakpoint Condition: ' })
--- nvim/.config/nvim/lua/utils/on_attach.lua
+++ nvim/.config/nvim/lua/utils/on_attach.lua
@@ -1,11 +1,40 @@
-local navic = require('nvim-navic')
-local helpers = require("utils.helpers")
 local buf = vim.lsp.buf
 
-local opts = { noremap = true, silent = true, }
-local extend = function(tbl, desc)
-  return vim.tbl_extend("error", tbl, desc)
+local ls_ws_folders = function()
+  print(vim.inspect(buf.list_workspace_folders()))
 end
+
+local custom_hover = function()
+  local hover = buf.hover
+  local winid = require('ufo').peekFoldedLinesUnderCursor()
+  if not winid then
+    hover()
+  end
+end
+
+local open_diagnostic = function()
+  vim.diagnostic.open_float(0, { border = "single" })
+end
+
+local prev_diagnostic = function()
+  vim.diagnostic.goto_prev({ float = { border = "single" } })
+end
+
+local next_diagnostic = function()
+  vim.diagnostic.goto_next({ float = { border = "single" } })
+end
+
+local format_file = function()
+  vim.lsp.buf.format({ async = true, })
+end
+
+local manage_loclist = function()
+  vim.diagnostic.setloclist({ open = false }) -- don't open and focus
+  local window = vim.api.nvim_get_current_win()
+  vim.cmd.lwindow()                           -- open+focus loclist if has entries, else close -- this is the magic toggle command
+  vim.api.nvim_set_current_win(window)        -- restore focus to window you were editing (delete this if you want to stay in loclist)
+end
+
 
 -- reserve on attach key remaps for nvim-lsp specific commands
 return function(client, bufnr)
@@ -13,26 +42,27 @@ return function(client, bufnr)
   vim.api.nvim_set_option_value('omnifunc', 'v:lua.vim.lsp.omnifunc', { buf = bufnr })
 
   -- Mappings.
-  vim.keymap.set('n', 'gD', buf.declaration, extend(opts, { desc = "Buffer declarations" }))
-  vim.keymap.set('n', 'gd', buf.definition, extend(opts, { desc = "Go to definition" }))
-  vim.keymap.set('n', 'K', helpers.custom_hover, extend(opts, { desc = "View definition" }))
-  vim.keymap.set('n', 'gi', buf.implementation, extend(opts, { desc = "Get implementation" }))
-  vim.keymap.set('n', 'SH', buf.signature_help, extend(opts, { desc = "Signature help" }))
-  vim.keymap.set('n', '<space>wa', buf.add_workspace_folder, extend(opts, { desc = "Add workspace to folder" }))
-  vim.keymap.set('n', '<space>wr', buf.remove_workspace_folder, extend(opts, { desc = "remove workspace from folder" }))
-  vim.keymap.set('n', '<space>wl', helpers.ls_ws_folders, extend(opts, { desc = "List workspace folders" }))
-  vim.keymap.set('n', '<space>D', buf.type_definition, extend(opts, { desc = "Type definition" }))
-  vim.keymap.set('n', '<space>rn', buf.rename, extend(opts, { desc = "Rename" }))
-  vim.keymap.set('n', '<space>ca', buf.code_action, extend(opts, { desc = "Code action" }))
-  vim.keymap.set('n', 'gr', buf.references, extend(opts, { desc = "Get references" }))
-  vim.keymap.set('n', '<space>e', helpers.open_diagnostic, extend(opts, { desc = "Open diagnostic on current line" }))
-  vim.keymap.set('n', '[d', helpers.next_diagnostic, extend(opts, { desc = "Jump to next diagnostic" }))
-  vim.keymap.set('n', ']d', helpers.prev_diagnostic, extend(opts, { desc = "Jump to previous diagnostic" }))
-  vim.keymap.set('n', '<space>q', helpers.manage_loclist, extend(opts, { desc = "Set diagnostic list" }))
-  vim.keymap.set('n', '<space>f', helpers.format_file, extend(opts, { desc = "Format file" }))
+  vim.keymap.set('n', 'gD', buf.declaration, { desc = "Buffer declarations" })
+  vim.keymap.set('n', 'gd', buf.definition, { desc = "Go to definition" })
+  vim.keymap.set('n', 'K', custom_hover, { desc = "View definition" })
+  vim.keymap.set('n', 'gi', buf.implementation, { desc = "Get implementation" })
+  vim.keymap.set('n', 'SH', buf.signature_help, { desc = "Signature help" })
+  vim.keymap.set('n', '<space>wa', buf.add_workspace_folder, { desc = "Add workspace to folder" })
+  vim.keymap.set('n', '<space>wr', buf.remove_workspace_folder, { desc = "remove workspace from folder" })
+  vim.keymap.set('n', '<space>wl', ls_ws_folders, { desc = "List workspace folders" })
+  vim.keymap.set('n', '<space>D', buf.type_definition, { desc = "Type definition" })
+  vim.keymap.set('n', '<space>rn', buf.rename, { desc = "Rename" })
+  vim.keymap.set('n', '<space>ca', buf.code_action, { desc = "Code action" })
+  vim.keymap.set('n', 'gr', buf.references, { desc = "Get references" })
 
-  -- nvim navic for lualine
-  if client.server_capabilities.documentSymbolProvider then
-    navic.attach(client, bufnr)
-  end
+  vim.keymap.set('n', '<space>e', open_diagnostic, { desc = "Open diagnostic on current line" })
+  vim.keymap.set('n', '[d', next_diagnostic, { desc = "Jump to next diagnostic" })
+  vim.keymap.set('n', ']d', prev_diagnostic, { desc = "Jump to previous diagnostic" })
+  vim.keymap.set('n', '<space>q', manage_loclist, { desc = "Set diagnostic list" })
+  vim.keymap.set('n', '<space>f', format_file, { desc = "Format file" })
+
+  -- -- nvim navic for lualine
+  -- if client.server_capabilities.documentSymbolProvider then
+  --   require("nvim-navic").attach(client, bufnr)
+  -- end
 end
