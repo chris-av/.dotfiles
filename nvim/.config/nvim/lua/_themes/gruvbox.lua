30a03931342b11cc1d301d6844f30068bc83aea3
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-06-22 16:58:23 -0700

    must have both gruvbox and snazzy

--- nvim/.config/nvim/lua/_themes/gruvbox.lua
+++ nvim/.config/nvim/lua/_themes/gruvbox.lua
@@ -131,3 +131,4 @@ M.lualine = {
 }
 
 return M
+
--- nvim/.config/nvim/lua/_themes/init.lua
+++ nvim/.config/nvim/lua/_themes/init.lua
@@ -5,6 +5,7 @@ return {
   evil = require("_themes.evil"),
   fluoromachine = require("_themes.fluoromachine"),
   grey = require("_themes.grey"),
+  gruvbox = require("_themes.gruvbox"),
   habamin = require("_themes.habamin"),
   matrix = require("_themes.matrix"),
   snazzy = require("_themes.snazzy"),
