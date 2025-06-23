a83cf6b411fd76baa9bcbeaa7adf303d4575d9b2
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-06-22 20:30:51 -0700

    add habamin tmux theme

---
+++ tmux/.config/tmux/utils/themes/habamin.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+pane_active_border_fg="#87d787"
+pane_border_fg="#9e9e9e"
+message_style_fg="#5faf5f"
+message_style_bg="#1c1c1c"
+status_style_fg="#5fafd7"
+status_style_bg="#1c1c1c"
+status_x_fg="#5faf5f"
+status_x_bg="#1c1c1c"
+status_y_fg="#5faf5f"
+status_y_bg="#1c1c1c"
+status_z_fg="#5faf5f"
+status_z_bg="#1c1c1c"
+flags_fg="#9e9e9e"
+
+# none text before separator
+window_x_fg="#1c1c1c"
+window_x_bg="#5faf5f"
+
+# other windows
+window_y_fg="#1c1c1c"
+window_y_bg="#5faf5f"
+
+# flag symbols
+window_z_bg="#1c1c1c"
+window_z_fg="#5faf5f"
+
+window_fmt_fg="#dadada"
+window_fmt_bg="#1c1c1c"
+
+# client_prefix_bg="#72F1B9"
+# client_prefix_fg="#1c1c1c"
+
+
+left_sep=""
+right_sep=""
+
+window_sel_fg="#262626"
+window_sel_bg="#767676"
