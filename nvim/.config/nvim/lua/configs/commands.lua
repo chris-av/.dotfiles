f3e55303552de48bce6facf7f3023eebc6bf4a34
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-07-05 14:32:36 -0700

    custom command to export nvim themes to json

--- nvim/.config/nvim/lua/configs/commands.lua
+++ nvim/.config/nvim/lua/configs/commands.lua
@@ -86,3 +86,38 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, { desc = "Format file" })
   end,
 })
+
+vim.api.nvim_create_user_command(
+  "ExportThemes",
+  function()
+    local palettes = {}
+    local uv = vim.uv
+    local themes = require("_themes")
+
+    for theme_name, theme in pairs(themes) do
+      if theme_name == "_defaults" then
+        goto skipping
+      end
+
+      palettes[theme_name] = {}
+      palettes[theme_name]["kitty"] = theme.kitty
+      palettes[theme_name]["theme"] = theme.theme
+
+      ::skipping::
+    end
+
+    local json_str = vim.fn.json_encode(palettes)
+    local home = os.getenv("HOME")
+    local filename = "nvim-themes.json"
+    local export_path = vim.fs.joinpath(home, ".cache", filename)
+    local fd = uv.fs_open(export_path, "w+", tonumber('660', 8))
+    if fd == nil then
+      print("fd is nil, exiting now")
+      return
+    end
+    uv.fs_write(fd, json_str, 0, function()
+      print("exported theme file to : " .. export_path)
+    end)
+  end,
+  { desc = "Export theme assets into a JSON file", }
+)
