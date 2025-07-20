4aacd0b0614bbfe3dc925ba5f42bebc11603e8a2
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-07-19 18:47:56 -0700

    update tmux statusline catpppuccin-latte theme

--- tmux/.config/tmux/utils/themes/catppuccin-latte.sh
+++ tmux/.config/tmux/utils/themes/catppuccin-latte.sh
@@ -1,39 +1,39 @@
 #!/usr/bin/env bash
-pane_active_border_fg="#ebdbb2"
-pane_border_fg="#665c54"
-message_style_fg="#ebdbb2"
-message_style_bg="#282828"
-status_style_fg="#ebdbb2"
-status_style_bg="#282828"
-status_x_fg="#ebdbb2"
-status_x_bg="#282828"
-status_y_fg="#ebdbb2"
-status_y_bg="#282828"
-status_z_fg="#ebdbb2"
-status_z_bg="#282828"
+pane_active_border_fg="#4C4F69"
+pane_border_fg="#8839EF"
+message_style_fg="#4C4F69"
+message_style_bg="#EFF1F5"
+status_style_fg="#4C4F69"
+status_style_bg="#EFF1F5"
+status_x_fg="#000000"
+status_x_bg="#EFF1F5"
+status_y_fg="#4C4F69"
+status_y_bg="#EFF1F5"
+status_z_fg="#4C4F69"
+status_z_bg="#DC8A78"
 flags_fg="#000000"
 
 # none text before separator
-window_x_fg="#282828"
+window_x_fg="#EFF1F5"
 window_x_bg="#ebdbb2"
 
 # other windows
-window_y_fg="#282828"
+window_y_fg="#4C4F69"
 window_y_bg="#ebdbb2"
 
 # flag symbols
-window_z_bg="#282828"
+window_z_bg="#EFF1F5"
 window_z_fg="#ebdbb2"
 
-window_fmt_fg="#ebdbb2"
-window_fmt_bg="#282828"
+window_fmt_fg="#4C4F69"
+window_fmt_bg="#EFF1F5"
 
-client_prefix_bg="#3c3836"
-client_prefix_fg="#ebdbb2"
+client_prefix_bg="#EFF1F5"
+client_prefix_fg="#7287FD"
 
 
 left_sep=""
 right_sep=""
 
-window_sel_fg="#eff1f5"
+window_sel_fg="#EFF1F5"
 window_sel_bg="#dc8a78"
