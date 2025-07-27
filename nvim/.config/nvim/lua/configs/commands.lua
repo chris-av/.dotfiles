897626b0747734e19c85975b4227f82ba6c7079c
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-07-26 17:02:09 -0700

    replace autocmd with on_attach handler

--- nvim/.config/nvim/lua/configs/commands.lua
+++ nvim/.config/nvim/lua/configs/commands.lua
@@ -36,8 +36,6 @@ vim.api.nvim_create_autocmd("TermOpen", {
   group = group,
 })
 
-local lsp_maps = vim.api.nvim_create_augroup("LspKeymaps", { clear = true })
-
 vim.api.nvim_create_user_command(
   "LspInfo",
   function()
@@ -45,69 +43,6 @@ vim.api.nvim_create_user_command(
   end,
   { desc = "Show lsp information in the current buffer" }
 )
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = lsp_maps,
-  desc = "LSP-specific keymaps",
-  callback = function(ev)
-    local buf = vim.lsp.buf
-
-    vim.keymap.set('n', 'gD', buf.declaration, { desc = "Buffer declarations" })
-    vim.keymap.set('n', 'gd', buf.definition, { desc = "Go to definition" })
-    vim.keymap.set('n', 'K', function()
-      local hover = buf.hover
-      local winid = require('ufo').peekFoldedLinesUnderCursor()
-      if not winid then
-        hover()
-      end
-    end, { desc = "View definition" })
-    vim.keymap.set('n', 'gi', buf.implementation, { desc = "Get implementation" })
-    vim.keymap.set('n', 'SH', buf.signature_help, { desc = "Signature help" })
-    vim.keymap.set('n', '<space>wa', buf.add_workspace_folder, { desc = "Add workspace to folder" })
-    vim.keymap.set('n', '<space>wr', buf.remove_workspace_folder, { desc = "remove workspace from folder" })
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(buf.list_workspace_folders()))
-    end, { desc = "List workspace folders" })
-    vim.keymap.set('n', '<space>D', buf.type_definition, { desc = "Type definition" })
-    vim.keymap.set('n', '<space>rn', buf.rename, { desc = "Rename" })
-    vim.keymap.set('n', '<space>ca', buf.code_action, { desc = "Code action" })
-    vim.keymap.set('n', 'gr', buf.references, { desc = "Get references" })
-
-    vim.keymap.set('n', '<space>e', function()
-      vim.diagnostic.open_float({ bufnr = 0, border = "single" })
-    end, { desc = "Open diagnostic on current line" })
-    vim.keymap.set('n', '[d', function()
-      vim.diagnostic.jump({ count = 1, float = { border = "single" } })
-    end, { desc = "Jump to next diagnostic" })
-    vim.keymap.set('n', ']d', function()
-      vim.diagnostic.jump({ count = -1, float = { border = "single" } })
-    end, { desc = "Jump to previous diagnostic" })
-    vim.keymap.set('n', '<space>q', function()
-      vim.diagnostic.setloclist({ open = false }) -- don't open and focus
-      local window = vim.api.nvim_get_current_win()
-      vim.cmd.lwindow()                           -- open+focus loclist if has entries, else close -- this is the magic toggle command
-      vim.api.nvim_set_current_win(window)        -- restore focus to window you were editing (delete this if you want to stay in loclist)
-    end, { desc = "Set diagnostic list" })
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format({ async = true, })
-    end, { desc = "Format file" })
-
-    -- nvim navic
-    local bufnr = ev.buf
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if not client then
-      print("no client id found")
-      return
-    end
-
-    local navic = require("nvim-navic")
-
-    if client.server_capabilities.documentSymbolProvider then
-      navic.attach(client, bufnr)
-    end
-
-  end,
-})
 
 vim.api.nvim_create_user_command(
   "ExportThemes",
--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -22,6 +22,59 @@ require('mason-lspconfig').setup({
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
 vim.lsp.config("*", {
   capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    local buf = vim.lsp.buf
+
+    -- keymaps
+    vim.keymap.set('n', 'gD', buf.declaration, { desc = "Buffer declarations" })
+    vim.keymap.set('n', 'gd', buf.definition, { desc = "Go to definition" })
+    vim.keymap.set('n', 'K', function()
+      local hover = buf.hover
+      local winid = require('ufo').peekFoldedLinesUnderCursor()
+      if not winid then
+        hover()
+      end
+    end, { desc = "View definition" })
+    vim.keymap.set('n', 'gi', buf.implementation, { desc = "Get implementation" })
+    vim.keymap.set('n', 'SH', buf.signature_help, { desc = "Signature help" })
+    vim.keymap.set('n', '<space>wa', buf.add_workspace_folder, { desc = "Add workspace to folder" })
+    vim.keymap.set('n', '<space>wr', buf.remove_workspace_folder, { desc = "remove workspace from folder" })
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(buf.list_workspace_folders()))
+    end, { desc = "List workspace folders" })
+    vim.keymap.set('n', '<space>D', buf.type_definition, { desc = "Type definition" })
+    vim.keymap.set('n', '<space>rn', buf.rename, { desc = "Rename" })
+    vim.keymap.set('n', '<space>ca', buf.code_action, { desc = "Code action" })
+    vim.keymap.set('n', 'gr', buf.references, { desc = "Get references" })
+    vim.keymap.set('n', '<space>e', function()
+      vim.diagnostic.open_float({ bufnr = 0, border = "single" })
+    end, { desc = "Open diagnostic on current line" })
+    vim.keymap.set('n', '[d', function()
+      vim.diagnostic.jump({ count = 1, float = { border = "single" } })
+    end, { desc = "Jump to next diagnostic" })
+    vim.keymap.set('n', ']d', function()
+      vim.diagnostic.jump({ count = -1, float = { border = "single" } })
+    end, { desc = "Jump to previous diagnostic" })
+    vim.keymap.set('n', '<space>q', function()
+      vim.diagnostic.setloclist({ open = false })
+      local window = vim.api.nvim_get_current_win()
+      vim.cmd.lwindow()
+      vim.api.nvim_set_current_win(window)
+    end, { desc = "Set diagnostic list" })
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format({ async = true, })
+    end, { desc = "Format file" })
+
+    -- nvim navic
+    if not client then
+      return
+    end
+    local navic = require("nvim-navic")
+    if client.server_capabilities.documentSymbolProvider then
+      navic.attach(client, bufnr)
+    end
+  end,
 })
