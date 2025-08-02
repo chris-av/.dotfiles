625e579f61ed807ed70e7f9aa4f12bd39b5c5a2a
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-08-01 19:43:57 -0700

    change CTRL-T FZF behavior

--- zsh/zsh.conf/consts.sh
+++ zsh/zsh.conf/consts.sh
@@ -68,7 +68,7 @@ fi
 
 # FZF configs
 export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_CTRL_T_COMMAND="tmux ls -F '#S'"
 export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
 
 # pipx
