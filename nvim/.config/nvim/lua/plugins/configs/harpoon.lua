16d11c49f819c6ae6276b2a102fb555f55af80a2
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-08-09 19:48:15 -0700

    add useful keymaps in harpoon window

--- nvim/.config/nvim/lua/plugins/configs/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/configs/harpoon.lua
@@ -7,6 +7,17 @@ return {
     local harpoon = require("harpoon")
     local notify = require("utils.notifications")
 
+    harpoon:extend({
+      UI_CREATE = function(cx)
+        vim.keymap.set("n", "<C-v>", function()
+          harpoon.ui:select_menu_item({ vsplit = true, })
+        end, { buffer = cx.bufnr })
+        vim.keymap.set("n", "<C-x>", function()
+          harpoon.ui:select_menu_item({ split = true, })
+        end, { buffer = cx.bufnr })
+      end,
+    })
+
     harpoon:setup({})
 
     local harpoon_add = function()
