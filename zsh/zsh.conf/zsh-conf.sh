1afcf4be8b740ef1454045133fd9c226f3f2893a
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-08-17 13:21:28 -0700

    refactor LS_COLORS for readibility

--- zsh/zsh.conf/zsh-conf.sh
+++ zsh/zsh.conf/zsh-conf.sh
@@ -25,18 +25,18 @@ zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 PROMPT='%n%{$reset_color%} in %{$fg[yellow]%}%c %{$reset_color%}% %{$fg[blue]%}${vcs_info_msg_0_}%{$reset_color%} '
 
 # set the colors of terminal output
-LS_COLORS='no=00;37'
-LS_COLORS+=":fi=00"
-LS_COLORS+=":di=01;34"
-LS_COLORS+=":ln=00;36"
-LS_COLORS+=":pi=01;33"
-LS_COLORS+=":so=01;35"
-LS_COLORS+=":bd=01;40;33"
-LS_COLORS+=":cd=34"
-LS_COLORS+=":su=34"
-LS_COLORS+=":sg=34"
-LS_COLORS+=":tw=34"
-LS_COLORS+=":ow=34"
+LS_COLORS="no=00;37"        # normal; global default
+LS_COLORS+=":fi=00"         # file
+LS_COLORS+=":di=01;34"      # directory
+LS_COLORS+=":ln=00;36"      # symlink
+LS_COLORS+=":pi=01;33"      # pipe / fifo
+LS_COLORS+=":so=01;35"      # socket
+LS_COLORS+=":bd=01;40;33"   # block device
+LS_COLORS+=":cd=01;34"         # char
+LS_COLORS+=":su=01;34"         # setuid
+LS_COLORS+=":sg=01;34"         # setgid
+LS_COLORS+=":tw=01;34"         # dir that is sticky + other-writable
+LS_COLORS+=":ow=01;34"         # dir that is NOT sticky but is other-writable
 export LS_COLORS
 
 eval "$(fzf --zsh)"
