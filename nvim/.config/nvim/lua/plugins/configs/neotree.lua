dcc3add79eab7715374fd02f59967aa3e63edb5a
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-08-23 22:56:07 -0700

    nuke extra icons

--- nvim/.config/nvim/lua/plugins/configs/neotree.lua
+++ nvim/.config/nvim/lua/plugins/configs/neotree.lua
@@ -24,10 +24,10 @@ return {
         git_status = {
           symbols = {
             -- Change type
-            added = " ", -- or "✚", but this is redundant info if you use git_status_colors on the name
-            modified = " ", -- or "", but this is redundant info if you use git_status_colors on the name
-            deleted = " ", -- this can only be used in the git_status source
-            renamed = "󰁕", -- this can only be used in the git_status source
+            added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted = "", -- this can only be used in the git_status source
+            renamed = "", -- this can only be used in the git_status source
             -- Status type
             untracked = "✗",
             ignored = "◌",
