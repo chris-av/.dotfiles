e4d35070d9351931e7ac4f6df9fea6f550a14210
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-08-23 22:55:55 -0700

    allow normal mode on text input

--- nvim/.config/nvim/lua/plugins/configs/neotree.lua
+++ nvim/.config/nvim/lua/plugins/configs/neotree.lua
@@ -70,6 +70,17 @@ return {
           ["C"] = "prev_git_modified",
         },
       },
+      event_handlers = {
+        {
+          event = "neo_tree_popup_input_ready",
+          ---@param args { bufnr: integer, winid: integer }
+          handler = function(args)
+            -- map <esc> to enter normal mode (by default closes prompt)
+            -- don't forget `opts.buffer` to specify the buffer of the popup.
+            vim.keymap.set("i", "<esc>", vim.cmd.stopinsert, { noremap = true, buffer = args.bufnr })
+          end,
+        },
+      },
     })
   end,
 }
