c51895128bb59a35c2fec120eb5bcc9f128a4464
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2026-07-05 11:22:40 -0700

    create statusline theme for evil sys theme

---
+++ tmux/.config/tmux/utils/themes/evil.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+pane_active_border_fg="#FF0000"
+pane_border_fg="#FF0000"
+message_style_fg="#FF0000"
+message_style_bg="#000000"
+status_style_fg="#FF0000"
+status_style_bg="#000000"
+status_x_fg="#FF0000"
+status_x_bg="#000000"
+status_y_fg="#FF0000"
+status_y_bg="#000000"
+status_z_fg="#FF0000"
+status_z_bg="#000000"
+flags_fg="#FF0000"
+
+# none text before separator
+window_x_fg="#000000"
+window_x_bg="#FF0000"
+
+# other windows
+window_y_fg="#000000"
+window_y_bg="#FF0000"
+
+# flag symbols
+window_z_bg="#FF0000"
+window_z_fg="#000000"
+
+window_fmt_fg="#FF0000"
+window_fmt_bg="#000000"
+
+client_prefix_bg="#FF0000"
+client_prefix_fg="#000000"
+
+
+# left_sep=""
+# right_sep=""
+left_sep=""
+right_sep=""
+
+window_sel_fg="#000000"
+window_sel_bg="#FF0000"
+
+# copy-mode search match highlighting
+copy_mode_match_fg="#000000"
+copy_mode_match_bg="#FF0000"
+copy_mode_current_match_fg="#000000"
+copy_mode_current_match_bg="#FF0000"
